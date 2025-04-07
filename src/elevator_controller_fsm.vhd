8a683c3ca260c6189598b76e5fc305f78b1e7db5
seanflynn04 <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2025-04-07 08:38:17 -0600

    update

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -95,9 +95,36 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
-  
+    f_Q_next <= s_floor1 when ((f_Q = s_floor1)and(i_up_down = '0')and(i_stop='0')) or
+                              ((f_Q = s_floor2)and(i_up_down = '0')and(i_stop='0')) or
+                              ((f_Q = s_floor1)and(i_up_down = '0')and(i_stop='1')) or
+                              ((f_Q = s_floor1)and(i_up_down = '1')and(i_stop='1')) else
+                              
+                s_floor2 when ((f_Q = s_floor2)and(i_up_down = '0')and(i_stop='1')) or
+                              ((f_Q = s_floor2)and(i_up_down = '1')and(i_stop='1')) or
+                              ((f_Q = s_floor1)and(i_up_down = '1')and(i_stop='0')) or
+                              ((f_Q = s_floor3)and(i_up_down = '0')and(i_stop='0')) else
+                              
+                s_floor3 when ((f_Q = s_floor3)and(i_up_down = '0')and(i_stop='1')) or
+                              ((f_Q = s_floor3)and(i_up_down = '1')and(i_stop='1')) or
+                              ((f_Q = s_floor2)and(i_up_down = '1')and(i_stop='0')) or
+                              ((f_Q = s_floor4)and(i_up_down = '0')and(i_stop='0')) else
+                        
+                s_floor4 when ((f_Q = s_floor4)and(i_up_down = '0')and(i_stop='1')) or
+                              ((f_Q = s_floor4)and(i_up_down = '1')and(i_stop='1')) or
+                              ((f_Q = s_floor3)and(i_up_down = '1')and(i_stop='0')) or
+                              ((f_Q = s_floor4)and(i_up_down = '1')and(i_stop='0')) else
+                              
+                              s_floor2;
+            
 	-- Output logic
-
+    with f_Q select
+        o_floor <= "0001" when s_floor1,
+                   "0010" when s_floor2,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   "0001" when others;
+        
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
